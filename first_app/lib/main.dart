3c9d74c08638e846ee66bcb8d4f07958ae1a8508
Haseop Lee <haservi@naver.com>
2023-09-26 20:55:04 +0900

    feat: 클래스 분리 학습

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -2,25 +2,34 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 159, 19, 80),
-                Color.fromARGB(255, 107, 68, 172),
-              ],
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello world',
-              style: TextStyle(color: Colors.amberAccent, fontSize: 28),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 159, 19, 80),
+            Color.fromARGB(255, 107, 68, 172),
+          ],
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello world',
+          style: TextStyle(color: Colors.amberAccent, fontSize: 28),
+        ),
+      ),
+    );
+  }
 }
