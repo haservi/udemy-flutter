fb053b733c9101b33cfcdef16913f5dc14d80fb4
Haseop Lee <haservi@naver.com>
2023-09-27 22:20:06 +0900

    feat: 매개변수를 할당 받아 사용하는 방법 학습

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,6 +1,9 @@
 import 'package:first_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
@@ -8,15 +11,13 @@ class GradientContainer extends StatelessWidget {
   Widget build(context) {
     return Container(
       decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 159, 19, 80),
-            Color.fromARGB(255, 107, 68, 172),
-          ],
-        ),
+        gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 159, 19, 80),
+          Color.fromARGB(255, 107, 68, 172),
+        ], begin: startAlignment, end: endAlignment),
       ),
       child: const Center(
-        child: StyledText(),
+        child: StyledText('Hello World Text'),
       ),
     );
   }
