26489f31dabb0b69ca2b5606bfc98ebfe5da2ff9
Haseop Lee <haservi@naver.com>
2023-10-04 23:47:34 +0900

    feat: 이미지를 화면에 띄우는 학습 코드 추가

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,4 +1,3 @@
-import 'package:first_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
 const startAlignment = Alignment.topLeft;
@@ -17,8 +16,11 @@ class GradientContainer extends StatelessWidget {
         gradient: LinearGradient(
             colors: [color1, color2], begin: startAlignment, end: endAlignment),
       ),
-      child: const Center(
-        child: StyledText('Hello World Text'),
+      child: Center(
+        child: Image.asset(
+          'assets/images/dice-1.png',
+          width: 200,
+        ),
       ),
     );
   }
