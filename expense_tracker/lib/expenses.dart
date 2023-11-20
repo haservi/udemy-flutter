48661cd5b6573f16d844d1d085e8ca8746067cdb
Haseop Lee <haservi@naver.com>
2023-11-20 23:12:23 +0900

    feat: Dummy Data 변수 추가

--- expense_tracker/lib/expenses.dart
+++ expense_tracker/lib/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -10,6 +11,21 @@ class Expenses extends StatefulWidget {
 }
 
 class _Expenses extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: '플러터 코스',
+      amount: 20.01,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: '영화 보기',
+      amount: 40,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
