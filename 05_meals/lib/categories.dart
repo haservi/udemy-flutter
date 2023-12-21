86c00a0f1ef260c09bca818d3cb297fda87b7f22
Haseop Lee <haservi@naver.com>
2023-12-21 22:14:36 +0900

    feat: 구글 폰트 적용 및 GridView 적용 학습

---
+++ 05_meals/lib/categories.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CategorieScreen extends StatelessWidget {
+  const CategorieScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('카테고리 선택'),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: const [
+          Text('1', style: TextStyle(color: Colors.white)),
+          Text('2', style: TextStyle(color: Colors.white)),
+          Text('3', style: TextStyle(color: Colors.white)),
+          Text('4', style: TextStyle(color: Colors.white)),
+          Text('5', style: TextStyle(color: Colors.white)),
+          Text('6', style: TextStyle(color: Colors.white)),
+        ],
+      ),
+    );
+  }
+}
