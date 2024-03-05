94d4b351db88fd15e0387fa798d2bf5a5ed9028d
Haseop Lee <haservi@naver.com>
2024-03-05 19:29:59 +0900

    feat: 아이템 추가 화면 추가 및 유효성 관련 코드 추가

--- 06_shopping_list/lib/widget/grocery_list.dart
+++ 06_shopping_list/lib/widget/grocery_list.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/widget/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
 
+  @override
+  State<StatefulWidget> createState() {
+    return _GroceryListState();
+  }
+}
+
+class _GroceryListState extends State<GroceryList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("식료품"),
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: const Icon(Icons.add),
+          )
+        ],
       ),
       body: ListView.builder(
+        itemCount: groceryItems.length,
         itemBuilder: (ctx, index) => ListTile(
           title: Text(groceryItems[index].name),
           leading: Container(
@@ -22,7 +37,14 @@ class GroceryList extends StatelessWidget {
             groceryItems[index].quantity.toString(),
           ),
         ),
-        itemCount: groceryItems.length,
+      ),
+    );
+  }
+
+  void _addItem() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItem(),
       ),
     );
   }
