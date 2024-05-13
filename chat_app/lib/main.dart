1767eae76e33b38ef3fccdd648dac41fbfcf5f24
Haseop Lee <haservi@naver.com>
2024-05-14 00:33:53 +0900

    init: 채팅 앱 화면 추가 학습

---
+++ chat_app/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:chat_app/screen/auth.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chatting',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 63, 17, 177),
+        ),
+      ),
+      home: const AuthScreen(),
+    );
+  }
+}
