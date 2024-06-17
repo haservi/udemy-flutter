c41b00aac0a09e9d47f8c3b52549c51c3fbb8324
Haseop Lee <haservi@naver.com>
2024-06-18 05:30:51 +0900

    feat: firebase 로그인 시 이미지 추가 로직 학습

--- chat_app/lib/screen/auth.dart
+++ chat_app/lib/screen/auth.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:chat_app/widgets/user_image_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
 final _firebase = FirebaseAuth.instance;
@@ -20,10 +23,13 @@ class _AuthScreenState extends State<AuthScreen> {
   var _enteredEmail = '';
   var _enteredPassword = '';
 
+  File? _selectedImage;
+
   void _submit() async {
     var isValid = _form.currentState!.validate();
 
-    if (!isValid) {
+    if (!isValid || _isLogin && _selectedImage == null) {
+      // error 메시지
       return;
     }
 
@@ -41,6 +47,15 @@ class _AuthScreenState extends State<AuthScreen> {
         var userCredentials = await _firebase.createUserWithEmailAndPassword(
             email: _enteredEmail, password: _enteredPassword);
         print(userCredentials.toString());
+
+        var storageRef = FirebaseStorage.instance
+            .ref()
+            .child('user_images')
+            .child('${userCredentials.user!.uid}.jpg');
+        await storageRef.putFile(_selectedImage!);
+
+        var url = await storageRef.getDownloadURL();
+        print(url);
       }
     } on FirebaseAuthException catch (error) {
       if (error.code == 'email-already-in-use') {
@@ -84,7 +99,10 @@ class _AuthScreenState extends State<AuthScreen> {
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
-                          if (!_isLogin) const UserImagePicker(),
+                          if (!_isLogin)
+                            UserImagePicker(onPickImage: (selectedImage) {
+                              _selectedImage = selectedImage;
+                            }),
                           TextFormField(
                             decoration: const InputDecoration(
                               labelText: 'Email Address',
